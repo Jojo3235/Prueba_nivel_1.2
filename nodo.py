ae2bb9108ec3a0984b2cea0712f73779b8e40f97
Jojo3235 <joralmand@gmail.com>
2023-03-12 19:05:06 +0100

    Función eliminar y existe añadidas

--- nodo.py
+++ nodo.py
@@ -1,19 +1,2 @@
 class Nodo(object):
     info, sig = None, None
-
-aux = Nodo()
-aux.info = "Primer nodo"
-palabra = input("Ingrese una palabra: ")
-naux = aux
-while (palabra != ""):
-    nodo = Nodo()
-    nodo.info = palabra
-    naux.sig = nodo
-    naux = nodo
-    palabra = input("Ingrese una palabra: ")
-
-# Se queda en bucle constante, seguir mirando como va
-
-while (aux is not None):
-    print(aux.info)
-    aux = aux.sig
--- polinomio.py
+++ polinomio.py
@@ -1,4 +1,4 @@
-import nodo 
+from nodo import Nodo
 
 class datoPolinomio(object):
     def __init__(self, valor, termino):
@@ -10,39 +10,36 @@ class Polinomio(object):
         self.termino_mayor = None
         self.grado = -1
 
-    @staticmethod
     def agregar_termino(polinomio, termino, valor):
-        aux = nodo.Nodo()
+        aux = Nodo()
         dato = datoPolinomio(valor, termino)
         aux.info = dato
-        if (termino > polinomio.grado):
+        if(termino > polinomio.grado):
             aux.sig = polinomio.termino_mayor
+            polinomio.termino_mayor = aux
             polinomio.grado = termino
         else: 
             actual = polinomio.termino_mayor
-            while (actual.sig is not None and termino < actual.sig.info.termino):
+            while(actual.sig is not None and termino < actual.sig.info.termino):
                 actual = actual.sig
             aux.sig = actual.sig
             actual.sig = aux
 
-    @staticmethod
     def modificar_termino(polinomio, termino, valor):
         aux = polinomio.termino_mayor
         while(aux is not None and aux.info.termino != termino):
             aux = aux.sig
         aux.info.valor = valor
 
-    @staticmethod
     def obtener_valor(polinomio, termino):
         aux = polinomio.termino_mayor
         while(aux is not None and aux.info.termino > termino):
             aux = aux.sig
-            if (aux is not None and aux.info.termino == termino):
-                return aux.info.valor
-            else:
-                return 0
+        if (aux is not None and aux.info.termino == termino):
+            return aux.info.valor
+        else:
+            return 0
             
-    @staticmethod
     def mostrar(polinomio):
         aux = polinomio.termino_mayor
         pol = ""
@@ -53,30 +50,89 @@ class Polinomio(object):
                 aux = aux.sig
         return pol
         
-    @staticmethod
     def sumar(polinomio1, polinomio2):
         paux = Polinomio()
-        mayor = polinomio1 if (polinomio1.grado > polinomio2.grafo) else polinomio2
+        mayor = polinomio1 if (polinomio1.grado > polinomio2.grado) else polinomio2
         for i in range(0, mayor.grado + 1):
             total = Polinomio.obtener_valor(polinomio1, i) + Polinomio.obtener_valor(polinomio2, i)
             if total != 0:
                 Polinomio.agregar_termino(paux, i, total)
         return paux
 
-    @staticmethod
     def multiplicar(polinomio1, polinomio2):
         paux = Polinomio()
-        pol1 = polinomio1.temino_mayor
-        while pol1 is not None: 
+        pol1 = polinomio1.termino_mayor
+        while(pol1 is not None): 
             pol2 = polinomio2.termino_mayor
-            while pol2 is not None:
+            while(pol2 is not None):
                 termino = pol1.info.termino + pol2.info.termino
                 valor = pol1.info.valor * pol2.info.valor
-                if Polinomio.obtener_valor(paux, termino) != 0:
-                    valor += Polinomio.obterner_valor(paux, termino)
+                if(Polinomio.obtener_valor(paux, termino) != 0):
+                    valor += Polinomio.obtener_valor(paux, termino)
                     Polinomio.modificar_termino(paux, termino, valor)
                 else:
                     Polinomio.agregar_termino(paux,termino,valor)
                 pol2 = pol2.sig
             pol1 = pol1.sig
         return paux
+    
+    def eliminar(polinomio, termino):
+        aux = polinomio.termino_mayor
+        if (aux is not None):
+            if (aux.info.termino == termino):
+                polinomio.termino_mayor = aux.sig
+                aux.sig = None
+            else:
+                while(aux.sig is not None and aux.sig.info.termino != termino):
+                    aux = aux.sig
+                if (aux.sig is not None and aux.sig.info.termino == termino):
+                    aux.sig = aux.sig.sig
+
+    def existe_termino(polinomio, termino):
+        aux = polinomio.termino_mayor
+        while(aux is not None and aux.info.termino != termino):
+            aux = aux.sig
+        if (aux is not None and aux.info.termino == termino):
+            return True
+        else:
+            return False
+
+
+#multiplicar dos polinomios
+#polinomio1 = 2x^2 + 3x^1 + 4x^0
+#polinomio2 = 5x^2 + 6x^1 + 7x^0
+
+polinomio1 = Polinomio()
+Polinomio.agregar_termino(polinomio1, 3, 5)
+print("Polinomio 1: ", Polinomio.mostrar(polinomio1))
+Polinomio.agregar_termino(polinomio1, 2, 2)
+print("Polinomio 1: ", Polinomio.mostrar(polinomio1))
+Polinomio.agregar_termino(polinomio1, 1, 3)
+print("Polinomio 1: ", Polinomio.mostrar(polinomio1))
+Polinomio.agregar_termino(polinomio1, 0, 4)
+print("Polinomio 1: ", Polinomio.mostrar(polinomio1))
+
+polinomio2 = Polinomio()
+Polinomio.agregar_termino(polinomio2, 2, 5)
+Polinomio.agregar_termino(polinomio2, 1, 6)
+Polinomio.agregar_termino(polinomio2, 0, 7)
+print("Polinomio 2: ", Polinomio.mostrar(polinomio2))
+
+polinomio3 = Polinomio.sumar(polinomio1, polinomio2)
+print("Polinomio 3: ", Polinomio.mostrar(polinomio3))
+polinomio4 = Polinomio.multiplicar(polinomio1, polinomio2)
+print("Polinomio 4: ", Polinomio.mostrar(polinomio4))
+Polinomio.eliminar(polinomio1, 3)
+print("Polinomio 1: ", Polinomio.mostrar(polinomio1))
+
+print(Polinomio.existe_termino(polinomio1, 2))
+
+# Output:
+# Polinomio 1:  +5x^3
+# Polinomio 1:  +5x^3+2x^2
+# Polinomio 1:  +5x^3+2x^2+3x^1
+# Polinomio 1:  +5x^3+2x^2+3x^1+4x^0
+# Polinomio 2:  +5x^2+6x^1+7x^0
+# Polinomio 3:  +5x^3+7x^2+9x^1+11x^0
+# Polinomio 4:  +25x^5+40x^4+50x^3+50x^2+42x^1+28x^0
+# Polinomio 1:  +2x^2+3x^1+4x^0
